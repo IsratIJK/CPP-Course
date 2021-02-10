458aed2106d48788a599260b58add1596a6a6f97
IsratIJK <iptu.jahan.amin@gmail.com>
2021-02-10 21:54:32 +0600

    Variables in cpp

---
+++ Variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    string name="Fahim";
+    int age=18;
+
+    cout<<"There is a boy named "<<name<<endl;
+    cout<<"I am not sure about his current age, maybe it's "<<age<<endl;
+    cout<<"He likes a girl named IPTY"<<endl;
+    cout<<"But he don't like his family memders"<<endl;
+    
+}
