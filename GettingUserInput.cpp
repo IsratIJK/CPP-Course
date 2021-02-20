b471c437ed11968a46931ee4ca01a1ee90f90e62
IsratIJK <iptu.jahan.amin@gmail.com>
2021-02-20 22:04:34 +0600

    Getting user input

--- GettingUserInput.cpp
+++ GettingUserInput.cpp
@@ -4,11 +4,11 @@ int main() {
     int age;
     cout<<"Enter your age: ";
     cin>>age;
-    cout<<"You are"<<age<<"yrs old";
+    cout<<"You are "<<age<<"yrs old"<<endl;
     char grade;
     cout<<"Enter your grade";
     cin>>grade;
-    cout<<"You have got"<<grade<<"in your exam";
+    cout<<"You have got "<<grade<<" in your exam"<<endl;
     cin.clear();
     cin.sync();
     string name;
